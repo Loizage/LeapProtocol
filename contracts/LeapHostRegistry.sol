ca7991367ad0d60ae81352465595b2a18379b831
Loizage <loizage@Loizages-MacBook-Pro.local>
2022-05-19 14:47:08 +0100

    Described logic for HostRegistry

--- contracts/LeapHostRegistry.sol
+++ contracts/LeapHostRegistry.sol
@@ -5,5 +5,13 @@ import "hardhat/console.sol";
 
 /// @custom:security-contact security-leap@mail.com
 contract HostRegistry {
+
+    // Register host (Stake NFT)
+
+    // Register host event / project (Twitter Space)
+
+    // Set reward for event (Transfer ERC20)
+
+    // Turn-off Leap rewards
     
 }
